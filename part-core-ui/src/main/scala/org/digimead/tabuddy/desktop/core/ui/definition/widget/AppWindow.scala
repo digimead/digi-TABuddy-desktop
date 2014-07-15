30b1e7c0c2aa1378c3565731042e0118420f00ff
Alexey Aksenov <ezh@ezh.msk.ru>
2014-07-15 13:03:08 +0400

    Implement bridge between AppWindow and WorkbenchWindow

--- part-core-ui/src/main/scala/org/digimead/tabuddy/desktop/core/ui/definition/widget/AppWindow.scala
+++ part-core-ui/src/main/scala/org/digimead/tabuddy/desktop/core/ui/definition/widget/AppWindow.scala
@@ -48,24 +48,28 @@ import akka.pattern.ask
 import java.util.UUID
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.{ Inject, Named }
-import org.digimead.digi.lib.aop.log
 import org.digimead.digi.lib.log.api.XLoggable
-import org.digimead.tabuddy.desktop.core.Messages
+import org.digimead.tabuddy.desktop.core.{ Core, Messages }
 import org.digimead.tabuddy.desktop.core.definition.Context
 import org.digimead.tabuddy.desktop.core.support.App
 import org.digimead.tabuddy.desktop.core.ui.UI
-import org.digimead.tabuddy.desktop.core.ui.block.{ Window, WindowConfiguration, WindowSupervisor }
-import org.digimead.tabuddy.desktop.core.ui.block.StackConfiguration
+import org.digimead.tabuddy.desktop.core.ui.block.{ StackConfiguration, Window, WindowConfiguration, WindowSupervisor }
 import org.digimead.tabuddy.desktop.core.ui.block.builder.WindowContentBuilder
 import org.digimead.tabuddy.desktop.core.ui.definition.ToolBarManager
+import org.eclipse.core.runtime.IAdaptable
+import org.eclipse.core.runtime.dynamichelpers.IExtensionTracker
 import org.eclipse.e4.core.contexts.Active
 import org.eclipse.e4.core.di.annotations.Optional
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow
 import org.eclipse.jface.action.{ CoolBarManager, StatusLineManager }
 import org.eclipse.jface.window.ApplicationWindow
 import org.eclipse.swt.{ SWT, SWTException }
 import org.eclipse.swt.custom.StackLayout
 import org.eclipse.swt.events.{ DisposeEvent, DisposeListener }
 import org.eclipse.swt.widgets.{ Composite, Control, Event, Listener, Shell }
+import org.eclipse.ui.{ IPageListener, IPartService, IPerspectiveListener, ISelectionService, IWorkbench, IWorkbenchPage, IWorkbenchWindow, PlatformUI }
+import org.eclipse.ui.internal.{ WorkbenchWindow ⇒ EWorkbenchWindow }
+import org.eclipse.ui.internal.services.IServiceLocatorCreator
 import scala.language.implicitConversions
 
 /**
@@ -204,12 +208,16 @@ class AppWindow @Inject() (val id: UUID, @Optional argInitialConfiguration: Wind
   }
   /** Initialize current window. */
   protected def initialize() {
+    // Set application wide window if there is no one
+    Option(Core.serviceContext.get(classOf[MWindow])) getOrElse
+      { Core.serviceContext.set(classOf[MWindow], MWindowWrapper) }
     addStatusLine()
     addCoolBar(SWT.FLAT | SWT.WRAP)
     addMenuBar()
     setBlockOnOpen(false)
     getCoolBarManager.setOverrides(ToolBarManager.Overrides)
     windowContext.set(classOf[AppWindow], this)
+    windowContext.set(classOf[IWorkbenchWindow].getName(), WorkbenchWindow)
   }
   /** On window active. */
   protected def onActive() = {}
@@ -276,6 +284,125 @@ class AppWindow @Inject() (val id: UUID, @Optional argInitialConfiguration: Wind
         }
   }
 
+  /**
+   * MWindow stub
+   */
+  object MWindowWrapper extends MWindow {
+    def getContributorURI(): String = ???
+    def getElementId(): String = ???
+    def getPersistedState(): java.util.Map[String, String] = ???
+    def getTags(): java.util.List[String] = ???
+    def getTransientData(): java.util.Map[String, Object] = ???
+    def setContributorURI(x$1: String): Unit = ???
+    def setElementId(x$1: String): Unit = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.commands.MBindings
+    def getBindingContexts(): java.util.List[org.eclipse.e4.ui.model.application.commands.MBindingContext] = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.ui.MContext
+    def getContext(): org.eclipse.e4.core.contexts.IEclipseContext = AppWindow.this.windowContext
+    def getProperties(): java.util.Map[String, String] = ???
+    def getVariables(): java.util.List[String] = ???
+    def setContext(x$1: org.eclipse.e4.core.contexts.IEclipseContext): Unit = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.ui.MElementContainer
+    def getChildren(): java.util.List[org.eclipse.e4.ui.model.application.ui.basic.MWindowElement] = ???
+    def getSelectedElement(): org.eclipse.e4.ui.model.application.ui.basic.MWindowElement = ???
+    def setSelectedElement(x$1: org.eclipse.e4.ui.model.application.ui.basic.MWindowElement): Unit = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.commands.MHandlerContainer
+    def getHandlers(): java.util.List[org.eclipse.e4.ui.model.application.commands.MHandler] = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.ui.MSnippetContainer
+    def getSnippets(): java.util.List[org.eclipse.e4.ui.model.application.ui.MUIElement] = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.ui.MUIElement
+    def getAccessibilityPhrase(): String = ???
+    def getContainerData(): String = ???
+    def getCurSharedRef(): org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder = ???
+    def getLocalizedAccessibilityPhrase(): String = ???
+    def getParent(): org.eclipse.e4.ui.model.application.ui.MElementContainer[org.eclipse.e4.ui.model.application.ui.MUIElement] = ???
+    def getRenderer(): Object = ???
+    def getVisibleWhen(): org.eclipse.e4.ui.model.application.ui.MExpression = ???
+    def getWidget(): Object = AppWindow.this
+    def isOnTop(): Boolean = ???
+    def isToBeRendered(): Boolean = ???
+    def isVisible(): Boolean = AppWindow.this.getShell().isVisible()
+    def setAccessibilityPhrase(x$1: String): Unit = ???
+    def setContainerData(x$1: String): Unit = ???
+    def setCurSharedRef(x$1: org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder): Unit = ???
+    def setOnTop(x$1: Boolean): Unit = ???
+    def setParent(x$1: org.eclipse.e4.ui.model.application.ui.MElementContainer[org.eclipse.e4.ui.model.application.ui.MUIElement]): Unit = ???
+    def setRenderer(x$1: Any): Unit = ???
+    def setToBeRendered(x$1: Boolean): Unit = ???
+    def setVisible(x$1: Boolean): Unit = ???
+    def setVisibleWhen(x$1: org.eclipse.e4.ui.model.application.ui.MExpression): Unit = ???
+    def setWidget(x$1: Any): Unit = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.ui.MUILabel
+    def getIconURI(): String = ???
+    def getLabel(): String = ???
+    def getLocalizedLabel(): String = ???
+    def getLocalizedTooltip(): String = ???
+    def getTooltip(): String = ???
+    def setIconURI(x$1: String): Unit = ???
+    def setLabel(x$1: String): Unit = ???
+    def setTooltip(x$1: String): Unit = ???
+
+    // Members declared in org.eclipse.e4.ui.model.application.ui.basic.MWindow
+    def getHeight(): Int = ???
+    def getMainMenu(): org.eclipse.e4.ui.model.application.ui.menu.MMenu = ???
+    def getSharedElements(): java.util.List[org.eclipse.e4.ui.model.application.ui.MUIElement] = ???
+    def getWidth(): Int = ???
+    def getWindows(): java.util.List[org.eclipse.e4.ui.model.application.ui.basic.MWindow] = ???
+    def getX(): Int = ???
+    def getY(): Int = ???
+    def setHeight(x$1: Int): Unit = ???
+    def setMainMenu(x$1: org.eclipse.e4.ui.model.application.ui.menu.MMenu): Unit = ???
+    def setWidth(x$1: Int): Unit = ???
+    def setX(x$1: Int): Unit = ???
+    def setY(x$1: Int): Unit = ???
+  }
+  /**
+   * WorkbenchWindow stub
+   */
+  object WorkbenchWindow extends EWorkbenchWindow(null, null) {
+    // There is IWorkbenchWindow, but code monkeys use hardcoded WorkbenchWindow instead, so IWorkbenchWindow is useless
+    // WorkbenchWindow has
+    //  public final Object getService(final Class key)
+    //  public final boolean hasService(final Class key)
+    // A lot of thanks to those bastards who wrote those code
+    // They didn't mark WorkbenchWindow with final at all...
+    // Fucking shit :-(
+    val serviceLocatorField = classOf[EWorkbenchWindow].getDeclaredField("serviceLocator")
+    if (!serviceLocatorField.isAccessible())
+      serviceLocatorField.setAccessible(true)
+    serviceLocatorField.set(this, {
+      val slc = PlatformUI.getWorkbench().getService(classOf[IServiceLocatorCreator]).asInstanceOf[IServiceLocatorCreator]
+      slc.createServiceLocator(PlatformUI.getWorkbench(), null, null, windowContext)
+    })
+
+    // Members declared in org.eclipse.ui.IPageService
+    override def addPageListener(x$1: IPageListener): Unit = ???
+    override def addPerspectiveListener(x$1: IPerspectiveListener): Unit = ???
+    override def removePageListener(x$1: IPageListener): Unit = ???
+    override def removePerspectiveListener(x$1: IPerspectiveListener): Unit = ???
+
+    // Members declared in org.eclipse.ui.IWorkbenchWindow
+    override def close(): Boolean = true
+    override def getActivePage(): IWorkbenchPage = null
+    override def getExtensionTracker(): IExtensionTracker = ???
+    override def getPages(): Array[IWorkbenchPage] = ???
+    override def getPartService(): IPartService = ???
+    override def getSelectionService(): ISelectionService = ???
+    override def getShell(): Shell = AppWindow.this.getShell()
+    override def getWorkbench(): IWorkbench = PlatformUI.getWorkbench()
+    override def isApplicationMenu(menuID: String): Boolean = false
+    override def openPage(input: IAdaptable): IWorkbenchPage = null
+    override def openPage(perspectiveId: String, input: IAdaptable): IWorkbenchPage = null
+    override def run(x$1: Boolean, x$2: Boolean, x$3: org.eclipse.jface.operation.IRunnableWithProgress): Unit = ???
+    override def setActivePage(x$1: org.eclipse.ui.IWorkbenchPage): Unit = ???
+  }
   override lazy val toString = "AppWindow[%08X]".format(id.hashCode())
 }
 
